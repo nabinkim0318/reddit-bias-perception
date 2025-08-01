66f288112ef4b8eb1b649752bcdd3e141af5882c
Nabin Kim <nabinkim0318@gmail.com>
2025-08-01 11:39:18 +0900

    updates test cases

--- analysis/bertopic_model.py
+++ analysis/bertopic_model.py
@@ -52,7 +52,7 @@ from config.config import (
 )
 from config.config import TOPIC_MODEL_PATH  # Path to save the trained BERTopic model
 from config.config import TOPIC_OUTPUT  # Output path for topic-level summary
-from config.vectorizer_config import vectorizer_model  # Custom vectorizer for BERTopic
+from utils.archive_vectorizer_config import vectorizer_model  # Custom vectorizer for BERTopic
 
 # Load environment variables and configure logging
 load_dotenv()
